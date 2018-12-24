f2344db976bfb0cd46de38fc39f55f56b6268616
Jared Forsyth <jared@jaredforsyth.com>
2018-12-23 22:40:09 -0700

    admin interface looking reasonable

--- src/admin.js
+++ src/admin.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react'
-import {css} from 'glamor'
-import {render} from 'react-dom'
-
+// @flow
+import React, { useState, useEffect } from "react";
+import { css } from "glamor";
+import { render } from "react-dom";
+import { ETIME } from "constants";
 
 /**
 Schema!
@@ -20,127 +21,344 @@ subjects: Array<string>,
 
 */
 
-const Binput = (props) => {
-  const [value, setValue] = useState(props.value);
-  useEffect(() => {
-    setValue(props.value);
-  }, [props.value])
-  return <input {...props} value={value} onChange={evt => setValue(evt.target.value)} onBlur={() => props.onChange(value)} />
-};
-
-
-const showEntry = (entry, change) => {
-  return <tr key={entry.id}>
-    <td>
-      <Binput type="number" min="1919" max="2019" value={entry.year} onChange={year => change({...entry, year})} />
-    </td>
-    <td>
-      <Binput value={new Date(entry.date).toLocaleDateString()} onChange={date => change({...entry, date: new Date(date).getTime()})} />
-    </td>
-    {entry.description ? <td>
-      <Binput value={entry.description.text} placeholder="Description" onChange={text => change({...entry, description: {...entry.description, text}})} />
-    </td> : null}
-    {entry.description ? <td>
-      {entry.description.subjects.map((subject, i) => (
-        <div>
-        <Binput style={{display: 'block'}} value={subject} placeholder="Subject name" onChange={
-            text => change({...entry, description: {
-             ...entry.description, subjects: entry.description.subjects.slice(0, i).concat([text]).concat(entry.description.subjects.slice(i + 1))
-                           }
-            })} />
-        <button onClick={() => change({...entry, description: {...entry.description, subjects: entry.description.subjects.slice(0, i).concat(entry.description.subjects.slice(i+1))}})}>remove</button>
-      </div>
-        ))}
-      <button onClick={() => change({...entry, description: {...entry.description, subjects: entry.description.subjects.concat('')}})}>Add subject</button>
-    </td> : null}
-    {entry.quote ? <td>
-      <Binput value={entry.quote.context} placeholder="Context" onChange={context => change({...entry, quote: {...entry.quote, context}})} />
-    </td> : null}
-  </tr>
+type Quote = {
+  context: string,
+  dialog: Array<{speaker: string, text: string}>,
 }
 
+type Description = {text: string, subjects: Array<string>}
+
+type Entry = {
+  id: string,
+  year: number,
+  date: number,
+  quote?: Quote,
+  description?: Description
+};
+
+const Binput = props => {
+  const [value, setValue] = useState(props.value);
+  useEffect(
+    () => {
+      setValue(props.value);
+    },
+    [props.value]
+  );
+  return (
+    <input
+      {...props}
+      value={value}
+      onChange={evt => setValue(evt.target.value)}
+      onBlur={() => value !== props.value ? props.onChange(value) : null}
+    />
+  );
+};
+
+const showQuote = (quote: Quote, change) => {
+  return <>
+    <td>
+      <Binput
+        value={quote.context}
+        placeholder="Context"
+        onChange={context =>
+          change({ ...quote, context })
+        }
+      />
+    </td>
+    <td>
+      {quote.dialog.map((line, i) => (
+        <div key={i}>
+          <Binput
+            // style={{ display: "block" }}
+            value={line.speaker}
+            placeholder="Speaker"
+            onChange={speaker =>
+              change({
+                ...quote,
+                dialog: quote.dialog
+                  .slice(0, i)
+                  .concat([{ ...line, speaker }])
+                  .concat(quote.dialog.slice(i + 1))
+              })
+            }
+          />
+          <Binput
+            style={{ width: "200px" }}
+            value={line.text}
+            placeholder="Quote"
+            onChange={text =>
+              change({
+                ...quote,
+                dialog: quote.dialog
+                  .slice(0, i)
+                  .concat([{ ...line, text }])
+                  .concat(quote.dialog.slice(i + 1))
+              })
+            }
+          />
+          {i !== 0 ? <button
+            onClick={() =>
+              change({
+                ...quote,
+                dialog: quote.dialog
+                  .slice(0, i)
+                  .concat(quote.dialog.slice(i + 1))
+              })
+            }
+          >
+            remove
+              </button> : null}
+        </div>
+      ))}
+      <button
+        onClick={() =>
+          change({
+            ...quote,
+            dialog: quote.dialog.concat({ speaker: '', text: '' })
+          })
+        }
+      >
+        Add response
+          </button>
+    </td>
+  </>
+
+};
+
+const showDescription = (description: Description, change) => {
+  return <>
+      <td>
+        <Binput
+          value={description.text}
+          placeholder="Description"
+          onChange={text =>
+            change({ ...description, text })
+          }
+        />
+      </td>
+      <td>
+        {description.subjects.map((subject, i) => (
+          <div key={i}>
+            <Binput
+              // style={{ display: "block" }}
+              value={subject}
+              placeholder="Subject name"
+              onChange={text =>
+                change({
+                    ...description,
+                    subjects: description.subjects
+                      .slice(0, i)
+                      .concat([text])
+                      .concat(description.subjects.slice(i + 1))
+                })
+              }
+            />
+            <button
+              onClick={() =>
+                change({
+                    ...description,
+                    subjects: description.subjects
+                      .slice(0, i)
+                      .concat(description.subjects.slice(i + 1))
+                })
+              }
+            >
+              remove
+          </button>
+          </div>
+        ))}
+        <button
+          onClick={() =>
+            change({
+                ...description,
+                subjects: description.subjects.concat("")
+            })
+          }
+        >
+          Add subject
+      </button>
+      </td>
+  </>
+};
+
+const showEntry = (entry: Entry, change) => {
+  return (
+    <>
+    <tr key={entry.id}>
+      <td>
+        <Binput
+          type="number"
+          min="1919"
+          max="2019"
+          value={'' + entry.year}
+          onChange={year => change({ ...entry, year: parseInt(year) })}
+        />
+      </td>
+      <td>
+        <Binput
+          value={new Date(entry.date).toLocaleDateString()}
+          onChange={date =>
+            change({ ...entry, date: new Date(date).getTime() })
+          }
+        />
+      </td>
+      {entry.description ? showDescription(entry.description, description => change({...entry, description})) : null}
+      {entry.quote ? showQuote(entry.quote, quote => change({...entry, quote})) : null}
+    </tr>
+    <tr><td style={{height: '20px'}}></td></tr>
+    </>
+  );
+};
 
 // const attrs = 'id, context, dialog, year, date'.split(', ');
 
-const showQuotes = quotes => {
-  return <table>
-    <thead>
-      <tr>
-        {attrs.slice(1).map(attr => <th key={attr}>{attr}</th>)}
-      </tr>
-    </thead>
-    <tbody>
-    {quotes.map((data) => (
-      <tr key={data.id}>
-        {attrs.slice(1).map(attr => <td key={attr}>{attr === 'date' ? new Date(data[attr]).toLocaleDateString() : data[attr]}</td>)}
-      </tr>
-    ))}
-    </tbody>
-  </table>;
+const showQuotes = (quotes, change) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {['year', 'date', 'context/description', 'dialog/subjects'].map(attr => (
+            <th key={attr}>{attr}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {quotes.map((data, i) => showEntry(data, data => change(quotes.slice(0, i).concat([data]).concat(quotes.slice(i + 1)))))}
+      </tbody>
+    </table>
+  );
 };
 
-const View = ({password, go}) => {
+const View = ({ password, go }) => {
   const [quotes, setQuotes] = useState(null);
   useEffect(() => {
-    fetch('/quotes', {headers: {Authentication: 'Bearer ' + password}}).then(res => res.json()).then(setQuotes, err => setQuotes('failed'));
+    fetch("/quotes", { headers: { Authentication: "Bearer " + password } })
+      .then(res => res.json())
+      .then(setQuotes, err => setQuotes("failed"));
   }, []);
   if (quotes === null) {
-    return 'loading';
+    return "loading";
   }
-  if (quotes === 'failed') {
-    return 'bad password'
+  if (quotes === "failed") {
+    return "bad password";
   }
-  return showQuotes(quotes)
+  return showQuotes(quotes, setQuotes);
 };
 
-const getId = () => Date.now().toString(36) + Math.random().toString(36).slice(1);
+const getId = () =>
+  Date.now().toString(36) +
+  Math.random()
+    .toString(36)
+    .slice(1);
 
-const parseBulk = text => {
-  const lines = text.split('\n').map(line => line.split(/\t/g)).map(items => {
-    const data = {id: getId()};
-    attrs.slice(1).forEach((attr, i) => {
-      let v = items[i] || '';
-      if (attr === 'date') {
-        v = v ? new Date(v).getTime() : Date.now();
+const toRows = text => text
+    .split("\n")
+    .map(line => line.split(/\t/g));
+    
+
+const parseBulk = rows => {
+  const lines = rows.map(items => {
+      const row: any = { id: getId() };
+      const attrs = 'id, speaker, context, quote, secondSpeaker, response, year, date'.split(', ');
+      attrs.slice(1).forEach((attr, i) => {
+        let v = items[i] || "";
+        if (attr === "date") {
+          v = v ? new Date(v).getTime() : Date.now();
+        }
+        row[attr] = v;
+      });
+      const data: Entry = {id: getId(), year: row.year, date: row.date}
+      if (row.context && !row.quote) {
+        data.description = {
+          text: row.context,
+          subjects: row.speaker ? [row.speaker] : [],
+        }
+      } else {
+        data.quote = {
+          context: row.context,
+          dialog: [{speaker: row.speaker, text: row.quote}].concat(row.secondSpeaker ? [{speaker: row.secondSpeaker, text: row.response}] : [])
+        }
       }
-      data[attr] = v
-    });
-    return data
-  });
-  return lines
-}
 
-const Insert = ({password, go}) => {
-  const [data, setData] = useState('');
+      return data;
+    });
+  return lines;
+};
+
+const Insert = ({ password, go }) => {
+  const [data, setData] = useState("");
+  const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
-  return <div>
-    <textarea value={data} onChange={evt => setData(evt.target.value)} />
-    {loading === true ? 'Loading...' : (loading === 'error' ? 'Failed :(' : null)}
-    <button onClick={evt => {
-        setLoading(true);
-        fetch('/quotes/bulk', {method: 'POST', headers: {Authentication: 'Bearer ' + password, 'Content-Type': 'application/json'}, body: JSON.stringify(parseBulk(data))})
-        .then(res => res.status === 204 ? setLoading(false) : setLoading('error'), err => setLoading('error'))
-      }}>Insert</button>
-    {showQuotes(parseBulk(data))}
-  </div>
+  return (
+    <div>
+      <textarea value={data}
+      onPaste={evt => {
+        if (evt.clipboardData.types.includes('text/html')) {
+          evt.preventDefault();
+          evt.stopPropagation();
+          const data = evt.clipboardData.getData('text/html')
+          const node = document.createElement('div')
+          node.innerHTML = data
+          const rows = [...node.querySelectorAll('tr')].map(tr => [...tr.querySelectorAll('td')].map(td => td.textContent))
+          setQuotes(parseBulk(rows))
+        }
+      }}
+      onChange={evt => {
+        setData(evt.target.value)
+        setQuotes(parseBulk(toRows(evt.target.value)))
+      }} />
+      {loading === true
+        ? "Loading..."
+        : loading === "error"
+        ? "Failed :("
+        : null}
+      <button
+        onClick={evt => {
+          setLoading(true);
+          fetch("/quotes/bulk", {
+            method: "POST",
+            headers: {
+              Authentication: "Bearer " + password,
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(quotes)
+          }).then(
+            res =>
+              res.status === 204 ? setLoading(false) : setLoading("error"),
+            err => setLoading("error")
+          );
+        }}
+      >
+        Insert
+      </button>
+      {showQuotes(quotes, setQuotes)}
+    </div>
+  );
 };
 
 const Admin = () => {
-  const [password, setPassword] = useState('')
+  const [password, setPassword] = useState("");
   const [route, go] = useState(null);
-  
-  return <div>
-    <input onChange={evt => setPassword(evt.target.value)} value={password} placeholder="password"/>
-    <button onClick={() => go('view')}>
-      Show All
-    </button>
-    <button onClick={() => go('insert')}>
-      Bulk Insert
-    </button>
-    {route === 'view' ? <View password={password} go={go} /> : (route === 'insert' ? <Insert password={password} go={go} /> : null)}
-  </div>
+
+  return (
+    <div>
+      <input
+        onChange={evt => setPassword(evt.target.value)}
+        value={password}
+        placeholder="password"
+      />
+      <button onClick={() => go("view")}>Show All</button>
+      <button onClick={() => go("insert")}>Bulk Insert</button>
+      {route === "view" ? (
+        <View password={password} go={go} />
+      ) : route === "insert" ? (
+        <Insert password={password} go={go} />
+      ) : null}
+    </div>
+  );
 };
 
-
-
-render(<Admin />, document.getElementById('admin'));
+const node = document.getElementById("admin")
+if (node) {
+  render(<Admin />, node);
+}
--- src/index.js
+++ src/index.js
@@ -1,137 +1,233 @@
-import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import { css } from 'glamor'
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { css } from "glamor";
 
-const check = password => fetch('/check', {headers: {'Authentication': 'Bearer ' + password}}).then(res => res.status === 204);
-const getQuotes = () => fetch('/quotes', {headers: {'Authentication': 'Bearer ' + localStorage.password}}).then(res => res.json());
-const submitQuote = (quote) => fetch('/quotes', {method: 'PUT', body: JSON.stringify(quote), headers: {'Content-Type': 'application/json', 'Authentication': 'Bearer ' + localStorage.password}}).then(res => {
-  if (res.status !== 204) {
-    throw new Error('Invalid response: ' + res.status);
-  }
-});
+const check = password =>
+  fetch("/check", { headers: { Authentication: "Bearer " + password } }).then(
+    res => res.status === 204
+  );
+const getQuotes = () =>
+  fetch("/quotes", {
+    headers: { Authentication: "Bearer " + localStorage.password }
+  }).then(res => res.json());
+const submitQuote = quote =>
+  fetch("/quotes", {
+    method: "PUT",
+    body: JSON.stringify(quote),
+    headers: {
+      "Content-Type": "application/json",
+      Authentication: "Bearer " + localStorage.password
+    }
+  }).then(res => {
+    if (res.status !== 204) {
+      throw new Error("Invalid response: " + res.status);
+    }
+  });
 
-function Login({onLogin}) {
-  const [value, setValue] = useState('');
+function Login({ onLogin }) {
+  const [value, setValue] = useState("");
   const [error, setError] = useState(false);
-  return <div className={css({padding: 20})}>
-    <TextField autoFocus error={error} value={value} onChange={evt => {
-        setValue(evt.target.value)
-        setError(false)
-      }} placeholder="Enter password" />
-    <Button variant="contained" color="primary" onClick={() => {
-        check(value).then(success => {
-          if (success) {
-            onLogin(value)
-          } else {
-            setError(true)
-          }
-        })
-      }}>
-      Login
-    </Button>
-  </div>
+  return (
+    <div className={css({ padding: 20 })}>
+      <TextField
+        autoFocus
+        error={error}
+        value={value}
+        onChange={evt => {
+          setValue(evt.target.value);
+          setError(false);
+        }}
+        placeholder="Enter password"
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          check(value).then(success => {
+            if (success) {
+              onLogin(value);
+            } else {
+              setError(true);
+            }
+          });
+        }}
+      >
+        Login
+      </Button>
+    </div>
+  );
 }
 
 function renderRandom(quotes) {
   if (quotes == null) {
-    return 'Loading...'
+    return "Loading...";
   }
   if (!quotes.length) {
-    return 'No quotes'
+    return "No quotes";
   }
-  const index = parseInt(Math.random() * quotes.length)
-  const {speaker, context, secondSpeaker, quote, response, date} = quotes[index];
-  return <div>
-    <div style={{fontWeight: 'bold', color: '#999'}}>{speaker}</div>
-    <div>{quote}</div>
-  </div>
+  const index = parseInt(Math.random() * quotes.length);
+  const { speaker, context, secondSpeaker, quote, response, date } = quotes[
+    index
+  ];
+  return (
+    <div>
+      <div style={{ fontWeight: "bold", color: "#999" }}>{speaker}</div>
+      <div>{quote}</div>
+    </div>
+  );
 }
 
-function Random({onClose}) {
+function Random({ onClose }) {
   const [quotes, setQuotes] = useState(null);
   useEffect(() => {
-    getQuotes().then(quotes => setQuotes(quotes)); 
+    getQuotes().then(quotes => setQuotes(quotes));
   }, []);
-  return <div className={css({flex: 1, position: 'absolute',
-                              top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'white'})}>
-    {renderRandom(quotes)}
-    <Button onClick={onClose}>
-    Close
-    </Button>
-  </div>
+  return (
+    <div
+      className={css({
+        flex: 1,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: "white"
+      })}
+    >
+      {renderRandom(quotes)}
+      <Button onClick={onClose}>Close</Button>
+    </div>
+  );
 }
 
-function QuoteForm({onSubmit, data={}}) {
-  const [speaker, setSpeaker] = useState(data.speaker || '');
-  const [context, setContext] = useState(data.context || '');
-  const [secondSpeaker, setSecondSpeaker] = useState(data.secondSpeaker || '');
-  const [quote, setQuote] = useState(data.quote || '');
-  const [response, setResponse] = useState(data.response || '');
+function QuoteForm({ onSubmit, data = {} }) {
+  const [speaker, setSpeaker] = useState(data.speaker || "");
+  const [context, setContext] = useState(data.context || "");
+  const [secondSpeaker, setSecondSpeaker] = useState(data.secondSpeaker || "");
+  const [quote, setQuote] = useState(data.quote || "");
+  const [response, setResponse] = useState(data.response || "");
   const [date, setDate] = useState(data.date || Date.now());
   const [year, setYear] = useState(data.year || new Date().getFullYear());
-  return <div className={css({flex: 1, overflow: 'auto', display: 'flex', flexDirection: 'column', padding: 20})}>
-    <TextField autoFocus value={speaker} onChange={evt => {
-        setSpeaker(evt.target.value)
-      }} label="Speaker" />
-    <TextField multiline value={context} onChange={evt => {
-        setContext(evt.target.value)
-      }} label="Context" />
-    
-    <TextField multiline value={quote} onChange={evt => {
-        setQuote(evt.target.value)
-      }} label="Quote" />
-    
-    <TextField value={secondSpeaker} onChange={evt => {
-        setSecondSpeaker(evt.target.value)
-      }} label="Second Speaker" />
-    
-    <TextField multiline value={year + ''} onChange={evt => {
-        setYear(evt.target.value)
-      }} label="Response" />
-    
-    <TextField value={response} onChange={evt => {
-        setResponse(evt.target.value)
-      }} label="Year" />
-    
-    <Button onClick={() => {
-      onSubmit({speaker, context, secondSpeaker, quote, response, date})
-    }}>
-      Submit
-    </Button>
-    
-  </div>
+  return (
+    <div
+      className={css({
+        flex: 1,
+        overflow: "auto",
+        display: "flex",
+        flexDirection: "column",
+        padding: 20
+      })}
+    >
+      <TextField
+        autoFocus
+        value={speaker}
+        onChange={evt => {
+          setSpeaker(evt.target.value);
+        }}
+        label="Speaker"
+      />
+      <TextField
+        multiline
+        value={context}
+        onChange={evt => {
+          setContext(evt.target.value);
+        }}
+        label="Context"
+      />
+
+      <TextField
+        multiline
+        value={quote}
+        onChange={evt => {
+          setQuote(evt.target.value);
+        }}
+        label="Quote"
+      />
+
+      <TextField
+        value={secondSpeaker}
+        onChange={evt => {
+          setSecondSpeaker(evt.target.value);
+        }}
+        label="Second Speaker"
+      />
+
+      <TextField
+        multiline
+        value={year + ""}
+        onChange={evt => {
+          setYear(evt.target.value);
+        }}
+        label="Response"
+      />
+
+      <TextField
+        value={response}
+        onChange={evt => {
+          setResponse(evt.target.value);
+        }}
+        label="Year"
+      />
+
+      <Button
+        onClick={() => {
+          onSubmit({ speaker, context, secondSpeaker, quote, response, date });
+        }}
+      >
+        Submit
+      </Button>
+    </div>
+  );
 }
 
-const getId = () => Date.now().toString(36) + Math.random().toString(36).slice(1);
+const getId = () =>
+  Date.now().toString(36) +
+  Math.random()
+    .toString(36)
+    .slice(1);
 
 function QuoteAdder() {
   const [saving, setSaving] = useState(false);
   const [saveCount, setCount] = useState(0);
-  return <div>
-    {saving === true ? 'Saving...' : (saving ? 'Failed to submit quote :( check your connection and try again.' : null)}
-    <QuoteForm
-      key={'form-' + saveCount}
-    onSubmit={quote => {
-      setSaving(true);
-      submitQuote({...quote, id: getId()}).then(() => {
-        setSaving(false)
-        setCount(saveCount + 1);
-      }, err => setSaving(err));
-    }}/>
-  </div>
+  return (
+    <div>
+      {saving === true
+        ? "Saving..."
+        : saving
+        ? "Failed to submit quote :( check your connection and try again."
+        : null}
+      <QuoteForm
+        key={"form-" + saveCount}
+        onSubmit={quote => {
+          setSaving(true);
+          submitQuote({ ...quote, id: getId() }).then(
+            () => {
+              setSaving(false);
+              setCount(saveCount + 1);
+            },
+            err => setSaving(err)
+          );
+        }}
+      />
+    </div>
+  );
 }
 
 function Home() {
   const [showRandom, setShowRandom] = useState(false);
-  return <div className={css({
-      flex: 1,
-    })}>
-    <Button onClick={() => setShowRandom(true)}>Show a random quote</Button>
-    <QuoteAdder />
-    {showRandom ? <Random onClose={() => setShowRandom(false)} /> : null}
-  </div>
+  return (
+    <div
+      className={css({
+        flex: 1
+      })}
+    >
+      <Button onClick={() => setShowRandom(true)}>Show a random quote</Button>
+      <QuoteAdder />
+      {showRandom ? <Random onClose={() => setShowRandom(false)} /> : null}
+    </div>
+  );
 }
 
 function App() {
@@ -142,28 +238,35 @@ function App() {
       check(localStorage.password).then(success => {
         setLoading(false);
         if (success) {
-          setPassword(localStorage.password)
+          setPassword(localStorage.password);
         }
-      })
+      });
     }
-  }, [])
+  }, []);
   if (loading) {
-    return <div className={css({
-      flex: 1,
-      padding: 20,
-      textAlign: 'center',
-    })}>Loading...</div>
+    return (
+      <div
+        className={css({
+          flex: 1,
+          padding: 20,
+          textAlign: "center"
+        })}
+      >
+        Loading...
+      </div>
+    );
   }
   if (!password) {
-    return <Login onLogin={password => {
-      localStorage.password = password
-      setPassword(password);
-    }}/>
+    return (
+      <Login
+        onLogin={password => {
+          localStorage.password = password;
+          setPassword(password);
+        }}
+      />
+    );
   }
-  return (
-    <Home password={password} />
-  );
-  
+  return <Home password={password} />;
 }
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector("#app"));
